ea8becd206557427e2b4dacd81e2e838c7ca97ab
Eray Kurt <dequareta@gmail.com>
2021-05-17 19:11:00 +0300

    controller update

--- controller.py
+++ controller.py
@@ -1,1 +1,30 @@
-print("Sa")
+
+# Initiation of systems, options and variables --
+
+#code here
+
+
+#after this part infinity loop may be used
+#(these processes execute many times)
+
+
+# dormant state
+
+
+
+
+
+#verification state
+
+
+
+
+
+
+
+#deny state
+
+
+
+
+#unlock state
