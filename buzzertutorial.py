5fc9daba8e9aac439f576f8fb169a6a7f0b6afae
indibi <indibimert2@gmail.com>
2021-05-18 23:49:36 +0300

    minik syntax

--- buzzertutorial.py
+++ buzzertutorial.py
@@ -7,13 +7,13 @@ from time import sleep
 class Buzzer(object):
     def __init(self,pin_number):
         gpio.setmode(gpio.BOARD)
-        buzzer = GPIO.PWM(self.pin_number, 3000) # Set frequency to 1 Khz
+        self.buzzer = GPIO.PWM(self.pin_number, 3000) # Set frequency to 1 Khz
 
-    def ringwarning():
-        buzzer.start(40) # Set dutycycle to 10
+    def ringwarning(self):
+        self.buzzer.start(40) # Set dutycycle to 10
 
-    def ringerror():
-        buzzer.start(70) # Set dutycycle to 50
+    def ringerror(self):
+        self.buzzer.start(70) # Set dutycycle to 50
 
-    def stop():
-        buzzer.stop()
+    def stop(self):
+        self.buzzer.stop()
