d792d35b33272b65e626900aab55649640ba9809
Eray Kurt <dequareta@gmail.com>
2021-05-18 22:18:38 +0300

    update

--- buzzertutorial.py
+++ buzzertutorial.py
@@ -7,13 +7,13 @@ from time import sleep
 class Buzzer(object):
     def __init(self,pin_number):
         gpio.setmode(gpio.BOARD)
-        buzzer = GPIO.PWM(self.pin_number, 1000) # Set frequency to 1 Khz
+        buzzer = GPIO.PWM(self.pin_number, 3000) # Set frequency to 1 Khz
 
     def ringwarning():
-        buzzer.start(10) # Set dutycycle to 10
+        buzzer.start(40) # Set dutycycle to 10
 
     def ringerror():
-        buzzer.start(50) # Set dutycycle to 50
-        
+        buzzer.start(70) # Set dutycycle to 50
+
     def stop():
         buzzer.stop()
