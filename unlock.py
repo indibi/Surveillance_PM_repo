c12877aff7e1715f54a56e3d1446487338c73e94
Eray Kurt <dequareta@gmail.com>
2021-05-18 21:49:07 +0300

    unlock update

--- unlock.py
+++ unlock.py
@@ -1,23 +1,43 @@
 import RPi.GPIO as GPIO
 import time
-#define pins
-pins[4] = [35,36,37,38] #yanyana olan pinler (ayarlanabilir)
 
-for pin in pins:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin,0)
 
-fullstep_order =[[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
-# düz donüş
-for i in range(200): #200 variable'ı değiştirilebilir açısında göre
-    for fullstep in range(4):
-        for pin in range(4):
-            GPIO.output(pins[pin],fullstep_order[fullstep][pin])
-            time.sleep(0.001)
-time.sleep(25)
-# ters dönüş
-for i in range(200): #200 variable'ı değiştirilebilir açısında göre
-    for fullstep in range(4):
-        for pin in range(4):
-            GPIO.output(pins[pin],fullstep_order[3-fullstep][pin])
-            time.sleep(0.001)
+class Door(object):
+    def _init(self):
+        self.pin[4] = [35,36,37,38] #yanyana olan pinler (ayarlanabilir)
+        self.turn_number = 200 #dönme sayısı
+        self.fullstep_order =[[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+        for pin in pins:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin,0)
+
+
+    def entrance():
+    # düz donüş
+        for i in range(self.turn_number): #200 variable'ı değiştirilebilir açısında göre
+            for fullstep in range(4):
+                for pin in range(4):
+                    GPIO.output(pins[pin],fullstep_order[fullstep][pin])
+                    time.sleep(0.001)
+    #break beam kodu
+    # ters dönüş
+        for i in range(self.turn_number): #200 variable'ı değiştirilebilir açısında göre
+            for fullstep in range(4):
+                for pin in range(4):
+                    GPIO.output(pins[pin],fullstep_order[3-fullstep][pin])
+                    time.sleep(0.001)
+    def exit()
+        def entrance():
+        # düz donüş
+            for i in range(self.turn_number): #200 variable'ı değiştirilebilir açısında göre
+                for fullstep in range(4):
+                    for pin in range(4):
+                        GPIO.output(pins[pin],fullstep_order[fullstep][pin])
+                        time.sleep(0.001)
+        #break beam kodu
+        # ters dönüş
+            for i in range(self.turn_number): #200 variable'ı değiştirilebilir açısında göre
+                for fullstep in range(4):
+                    for pin in range(4):
+                        GPIO.output(pins[pin],fullstep_order[3-fullstep][pin])
+                        time.sleep(0.001)
