2e1568d8ecdbafb247bd2611a698af6e314f7dee
indibi <63194059+indibi@users.noreply.github.com>
2021-05-28 01:23:26 +0300

    asdadsdasdd

--- MaskDetector.py
+++ MaskDetector.py
@@ -53,13 +53,13 @@ class MaskDetector(object):
         (h,w) = frame.shape[:2]
         blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),(104.0, 177.0, 123.0))
         # pass the blob through the network and obtain the face detections
-    	faceNet.setInput(blob)
-    	detections = faceNet.forward()
-         # initialize our list of faces, their corresponding locations,
-     	# and the list of predictions from our face mask network
-     	faces = []
-     	locs = []
-     	preds = []
+        faceNet.setInput(blob)
+        detections = faceNet.forward()
+        # initialize our list of faces, their corresponding locations,
+        # and the list of predictions from our face mask network
+        faces = []
+        locs = []
+        preds = []
 
         # loop over the detections
         for i in range(0, detections.shape[2]):
