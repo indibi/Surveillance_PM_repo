e73cc728cce1e18f637414d2221f8537396480b1
indibi <63194059+indibi@users.noreply.github.com>
2021-05-28 08:34:05 +0300

    ereninki

--- MaskDetector.py
+++ MaskDetector.py
@@ -31,6 +31,28 @@ class MaskDetector(object):
         self.displayThread = threading.Thread(target=self.video_stream)
         self.displayThread.start()
 
+        self.image = cv2.imread(stock_image)
+        orig = self.image.copy()
+        (h, w) = self.image.shape[:2]
+        blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
+            (104.0, 177.0, 123.0))
+        self.faceNet.setInput(blob)
+        detections = self.faceNet.forward()
+        for i in range(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > 0.5:
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                (startX, startY) = (max(0, startX), max(0, startY))
+                (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+                face = image[startY:endY, startX:endX]
+                face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+                face = cv2.resize(face, (224, 224))
+                face = img_to_array(face)
+                face = preprocess_input(face)
+                face = np.expand_dims(face, axis=0)
+                self.maskNet.predict(face)
+
     def start_display(self):
         self.videoOn = True
         return self.videoStream.start()
