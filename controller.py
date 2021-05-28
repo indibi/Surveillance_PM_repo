73dad71a00fb47201b1f1b81d4288d0c58e262c2
indibi <63194059+indibi@users.noreply.github.com>
2021-05-28 03:15:32 +0300

    fghj

--- controller.py
+++ controller.py
@@ -1,17 +1,26 @@
 
 # Initiation of systems, objects and variables --
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import load_model
-from imutils.video import VideoStream
-from smbus2 import SMBus
-from mlx90614 import MLX90614
-import time, random, os
-import HCSR04
-import cv2
-import os
-import argparse
-import imutils
+# from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+# from tensorflow.keras.preprocessing.image import img_to_array
+# from tensorflow.keras.models import load_model
+# from imutils.video import VideoStream
+# from smbus2 import SMBus
+# from mlx90614 import MLX90614
+# import time, random, os
+# import HCSR04
+# import cv2
+# import os
+# import argparse
+# import imutils
+
+
+LOCKED = -10
+UNLOCKED = 100
+VERIFICATION = 10
+DORMANT = 50
+DENIED = 27
+STATE = DORMANT
+
 
 
 #code here
