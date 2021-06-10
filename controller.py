1ad0f565d1dde539838eb5f58bcf764ccf8ddfeb
indibi <63194059+indibi@users.noreply.github.com>
2021-06-11 00:52:43 +0300

    yorgun demokrat

--- controller.py
+++ controller.py
@@ -1,7 +1,7 @@
 # Initiation of systems, objects and variables --
 from Buzzer import Buzzer
 from Door import Door
-import PeopleCounter as pc
+import PeopleCounter
 import ExitHandReader
 import MaskDetector
 import OuterHandReader
@@ -33,7 +33,7 @@ door = Door()
 print("Door Initialized!")
 B = Buzzer(33)
 print("Buzzer Initialized!")
-PC = pc.PeopleCounter(23,24,21,22)
+PC = PeopleCounter.PeopleCounter(23,24,21,22)
 print("People Counter Initialized!")
 
 def main():
