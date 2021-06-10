9cf9110a73dbc9254089bfde46681f608f7329d0
indibi <63194059+indibi@users.noreply.github.com>
2021-06-11 02:03:43 +0300

    wcd

--- PC_tb.py
+++ PC_tb.py
@@ -1,10 +1,12 @@
 from PeopleCounter import PeopleCounter
+import RPi.GPIO as gpio
 import time
 
 X = PeopleCounter(23,24,21,22)
 try:
     while True:
         time.sleep(1)
+        print(f"BB1-(out/24 = {gpio.input(24)}, in/23 ={gpio.input(23)}), BB2-(out/23 = {gpio.input(22)}, in/23 = {gpio.input(21)})")
 except KeyboardInterrupt:
         X.__del__()
         exit(1)
