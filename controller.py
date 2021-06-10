4b4fce786ad266bf2f78101e649e13bcfad963c0
indibi <63194059+indibi@users.noreply.github.com>
2021-06-11 01:02:43 +0300

    asdfb

--- controller.py
+++ controller.py
@@ -1,7 +1,7 @@
 # Initiation of systems, objects and variables --
 from Buzzer import Buzzer
 from Door import Door
-from PeopleCounter import PeopleCounter
+import PeopleCounter
 import ExitHandReader
 import MaskDetector
 import OuterHandReader
