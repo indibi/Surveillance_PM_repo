c3107cdfb829ab05563f5ad05a02b61c2ff8e237
indibi <63194059+indibi@users.noreply.github.com>
2021-06-14 07:58:22 +0300

    sdsd

--- PC_tb.py
+++ PC_tb.py
@@ -6,7 +6,7 @@ X = PeopleCounter(23,24,21,22)
 try:
     while True:
         time.sleep(1)
-        print(f"BB1-(out/24 = {gpio.input(24)}, in/23 ={gpio.input(23)}), BB2-(out/23 = {gpio.input(22)}, in/23 = {gpio.input(21)})")
+        print(f"BB1-(out/24 = {gpio.input(24)}, in/23 ={gpio.input(23)}), BB2-(out/22 = {gpio.input(22)}, in/21 = {gpio.input(21)})")
 except KeyboardInterrupt:
         X.__del__()
         exit(1)
