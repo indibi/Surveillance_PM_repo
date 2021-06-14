56c35afcf33c69f6e31b8844ca96691311f5958c
indibi <63194059+indibi@users.noreply.github.com>
2021-06-14 10:39:18 +0300

    sadads

--- controller.py
+++ controller.py
@@ -15,7 +15,7 @@ def restart(channel):
     sleep(0.2)
     if gpio.input(channel) == gpio.LOW:
         print("Restarting!")
-        os.system("shutdown /r /t 1")
+        os.system("sudo shutdown now --reboot")
 
 def main():
     try:
