93048491348325cdb075d1fe1b278ec343913781
erengursu <45086356+erengursu@users.noreply.github.com>
2021-06-24 21:16:53 +0300

    Update train_mask_detector_3class.py
    changing BS size

--- train_mask_detector_3class.py
+++ train_mask_detector_3class.py
@@ -47,7 +47,7 @@ args = vars(ap.parse_args())
 # and batch size
 INIT_LR = 1e-4
 EPOCHS = 20
-BS = 32
+BS = 16
 
 # grab the list of images in our dataset directory, then initialize
 # the list of data (i.e., images) and class images
