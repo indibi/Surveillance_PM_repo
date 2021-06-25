da1c0c7ff1c1a4a9c8df1449b83f0abde7b7bf52
indibi <63194059+indibi@users.noreply.github.com>
2021-06-25 08:01:00 +0300

    asdasd

--- detect_mask_video_3class_v1.py
+++ detect_mask_video_3class_v1.py
@@ -93,7 +93,7 @@ ap.add_argument("-f", "--face", type=str,
 ap.add_argument("-m", "--model", type=str,
 	default="face_mask_detector.model",
 	help="path to trained face mask detector model")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
+ap.add_argument("-c", "--confidence", type=float, default=0.7,
 	help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
